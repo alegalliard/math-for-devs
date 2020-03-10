f0837c6ff5147008d45adacaaeb513b42a607ffa
Alessandra Souza <ale.sz.design@gmail.com>
2020-03-09 23:03:52 -0300

    draw random lines

---
+++ main.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight;
+
+        //context.fillRect(0, 0, width, height);
+    
+    for(var i = 0; i < 100; i += 1) {
+        context.beginPath();
+        context.moveTo(Math.random() * width, Math.random() * height);
+        context.lineTo(Math.random() * width, Math.random() * height);
+        context.stroke();
+    }
+};
