b8e60cdb3adcf75b11113cd5a8bd9555dac5e46e
Alessandra Souza <ale.sz.design@gmail.com>
2020-03-11 22:19:45 -0300

    🌈  colored random strokes

--- 01-random-strokes/main.js
+++ 01-random-strokes/main.js
@@ -3,13 +3,20 @@ window.onload = function() {
         context = canvas.getContext('2d'),
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight;
-
-        //context.fillRect(0, 0, width, height);
+    
+    var randomColor = function(max = 255) {
+        return Math.floor(Math.random() * (0 + max) + 1);
+    }
     
     for(var i = 0; i < 100; i += 1) {
+        var red = randomColor(100);
+        var green = randomColor();
+        var blue = randomColor();
+        
         context.beginPath();
         context.moveTo(Math.random() * width, Math.random() * height);
         context.lineTo(Math.random() * width, Math.random() * height);
+        context.strokeStyle = `rgb(${red},${green}, ${blue})`;
         context.stroke();
     }
-};
+};`
