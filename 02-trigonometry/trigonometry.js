46a3607114d1fc81491a89a8b8fd91e4e67d2e99
Alessandra Souza <ale.sz.design@gmail.com>
2020-03-11 22:52:49 -0300

    trigonometry 1

---
+++ 02-trigonometry/trigonometry.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight;
+
+    
+    context.translate(0, height / 2);
+    context.scale(1, -1);
+    for(var angle = 0; angle < Math.PI * 2; angle += .01) {
+        var x = angle * 100,
+            y = Math.sin(angle) * 200;
+        
+        context.fillRect(x, y, 5, 5);
+    }
+}
